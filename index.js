80148f2e6cbeee603181461b025df13e0409ec7b
Oberth Zolán Zsigmond <oberthzsigmond@gmail.com>
2019-04-17 08:45:08 +0200

    shader

--- index.js
+++ index.js
@@ -183,7 +183,38 @@ function makeSphere(scene, x, y, z) {
 
   // Add sphere geometry from three.js
   var geometry = new THREE.SphereGeometry(5, 15, 15);
-  var material = new THREE.MeshNormalMaterial({
+  // var material = new THREE.MeshLambertMaterial({
+  //   color: "red",
+  //   wireframe: true
+  // });
+  var material = new THREE.ShaderMaterial({
+    uniforms: {
+      color1: {
+        value: new THREE.Color("yellow")
+      },
+      color2: {
+        value: new THREE.Color("purple")
+      }
+    },
+    vertexShader: `
+      varying vec2 vUv;
+  
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+      }
+    `,
+    fragmentShader: `
+      uniform vec3 color1;
+      uniform vec3 color2;
+    
+      varying vec2 vUv;
+      
+      void main() {
+        
+        gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
+      }
+    `,
     wireframe: true
   });
   var sphere = new THREE.Mesh(geometry, material);
